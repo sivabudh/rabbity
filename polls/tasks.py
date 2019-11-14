338c353717a54ca7d74f1e88382f82d37a74aa23
Sivabudh Umpudh <sivabudh.u@codium.co>
2019-11-14 16:22:34 +0700

    Demonstrating Celery integration with Django model

--- polls/tasks.py
+++ polls/tasks.py
@@ -1,7 +1,12 @@
 from celery.task import task
+from django.contrib.auth.models import User
 
 
 @task(name="add_task")
 def add_task(a, b):
     print(f"Adding 2 numbers: {a} + {b}")
-    return a + b
+    user = User.objects.first()
+    if not user:
+        return a + b
+    else:
+        return user.email
--- polls/views.py
+++ polls/views.py
@@ -5,7 +5,7 @@ from rabbity.celery import app as celery_app
 
 @api_view(['GET'])
 def add(request):
-    response = celery_app.send_task('add_task', kwargs={'a': 4, 'b': 9})
+    response = celery_app.send_task('add_task', kwargs={'a': 5, 'b': 10})
     output = response.get()
     msg = f'Today is quite cool. Adding 2 numbers, we get our sum: {output}'
     return HttpResponse(msg, content_type='text/plain')
